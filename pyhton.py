392f6501dcb85b9ef4338cda7c3c20bff8d2622c
Mukkamala22 <162416154+Mukkamala22@users.noreply.github.com>
2024-03-12 10:56:53 +0000

    trail code

--- pyhton.py
+++ pyhton.py
@@ -162,3 +162,59 @@ import os
 
 os.remove("myfile.txt")
 
+import os
+
+def create_file(filename):
+    try:
+        with open(filename, "w") as file:
+                  file.write("hello,world!\n")
+        print("file " + filename + "created successfully.")
+    except IOError:
+        print()
+
+def read_file(filename):
+    try:
+        with open(filename, "r") as file:
+            content = file.read()
+            print(content)
+    except IOError:
+        print("error: could not rea file " + filename)
+
+def append_file(filename, text):
+    try:
+        with open(filename, "a") as file:
+            file.write(text)
+            print("text appended to file " + filename + " successfully.")
+    except IOError:
+        print("Error: could not append to file " + filename)
+              
+def rename_file(filename, new_filename):
+    try:
+        os.rename(filename, new_filename)
+        print("File " + filename + " renamed to " + new_filename + " successfully.")
+    except IOError:
+        print("Error: could not rename file " + filename)
+ 
+def delete_file(filename):
+    try:
+        os.remove(filename)
+        print("File " + filename + " deleted successfully.")
+    except IOError:
+        print("Error: could not delete file " + filename)
+
+if __name__ == '__main__':
+    filename = "example.txt"
+    new_filename = "new_example.txt"
+ 
+    create_file(filename)
+    read_file(filename)
+    append_file(filename, "This is some additional text.\n")
+    read_file(filename)
+    rename_file(filename, new_filename)
+    read_file(new_filename)
+    delete_file(new_filename)
+              
+    
+    
+
+
